deac1f5639ff50322b07fca197000a00c6c2ec93
Simon Fischer <fischer.sim@web.de>
2018-03-02 15:21:12 +0100

    added fitting functionality

--- npl/processing.py
+++ npl/processing.py
@@ -1,6 +1,20 @@
 """Provides functions for data processing."""
 
 import numpy as np
+from lmfit import Parameters
+from lmfit.models import PseudoVoigtModel
+
+
+def calculate_background(bgtype, energy, intensity):
+    """Returns background subtracted intensity."""
+    # pylint: disable=unsubscriptable-object
+    if bgtype == "linear":
+        background = np.linspace(intensity[0], intensity[-1], len(energy))
+    elif bgtype == "shirley":
+        background = shirley(energy, intensity)
+    else:
+        background = None
+    return background
 
 
 def shirley(energy, intensity, tol=1e-5, maxit=20):
@@ -9,6 +23,8 @@ def shirley(energy, intensity, tol=1e-5, maxit=20):
         is_reversed = True
         energy = energy[::-1]
         intensity = intensity[::-1]
+    else:
+        is_reversed = False
 
     background = np.ones(energy.shape) * intensity[-1]
     integral = np.zeros(energy.shape)
@@ -68,3 +84,111 @@ def normalize(intensity, norm):
     else:
         normto = max(intensity)
     return intensity / normto
+
+
+class RegionFitModelIface(object):
+    """This manages the Peak models and does the fitting."""
+    # pylint: disable=invalid-name
+    def __init__(self, region):
+        self.single_models = {}
+        self.params = Parameters()
+        self.region = region
+
+    @property
+    def total_model(self):
+        """Returns the sum of all models."""
+        if not self.single_models:
+            return None
+        model_list = list(self.single_models.values())
+        total = model_list[0]
+        for i in range(1, len(model_list)):
+            total += model_list[i]
+        return total
+
+    def add_peak(self, peak):
+        """Adds a new Peak to the Model list."""
+        if peak.region is not self.region:
+            raise ValueError("Peak does not belong to this Region")
+
+        if peak.model_name == "PseudoVoigt":
+            model = PseudoVoigtModel(prefix=peak.prefix)
+            model.set_param_hint("sigma", value=2, min=1e-5, max=5)
+            model.set_param_hint("amplitude", value=2000, min=0)
+            model.set_param_hint("fraction", vary=False)
+        self.single_models[peak.prefix] = model
+
+    def remove_peak(self, peak):
+        """Removes a Peak from the model and instantiates a new
+        CompositeModel."""
+        del self.single_models[peak.prefix]
+        for parname, param in self.params.items():
+            if peak.prefix in parname:
+                del param
+
+    def guess_params(self, peak):
+        """Guesses parameters for a new peak."""
+        model = self.single_models[peak.prefix]
+        y = (peak.region.intensity
+             - peak.region.background
+             - self.get_intensity())
+        params = model.guess(y, x=self.region.energy)
+        self.params += params
+
+    def init_params(self, peak, **kwargs):
+        """Sets initial values chosen by user."""
+        for parname in ("height", "fwhm", "center"):
+            if parname not in kwargs:
+                raise TypeError("Missing keyword argument {}".format(parname))
+
+        model = self.single_models[peak.prefix]
+        if peak.model_name == "PseudoVoigt":
+            sigma = kwargs["fwhm"] / 2
+            amplitude = (kwargs["height"]
+                         * (2 * sigma * np.sqrt(np.pi / np.log(2)))
+                         / (1 + np.sqrt(1 / (np.pi * np.log(2)))))
+            model.set_param_hint("sigma", value=sigma)
+            model.set_param_hint("amplitude", value=amplitude)
+            model.set_param_hint("center", value=kwargs["center"])
+            params = model.make_params()
+            self.params += params
+
+    def fit(self):
+        """Returns the fitted intensity values."""
+        if not self.single_models:
+            return
+        y = self.region.intensity - self.region.background
+        result = self.total_model.fit(y, self.params, x=self.region.energy)
+        self.params = result.params
+
+        for peak in self.region.peaks:
+            if peak.model_name == "PseudoVoigt":
+                amp = self.params["{}amplitude".format(peak.prefix)].value
+                sigma = self.params["{}sigma".format(peak.prefix)].value
+                center = self.params["{}center".format(peak.prefix)].value
+                peak.set(fwhm=sigma * 2)
+                peak.set(height=(amp
+                                 * (1 + np.sqrt(1 / (np.pi * np.log(2))))
+                                 / (2 * sigma * np.sqrt(np.pi / np.log(2)))))
+                peak.set(center=center)
+
+        # print(result.fit_report())
+
+    def get_peak_intensity(self, peak):
+        """Returns the model evaluation value for a given Peak."""
+        model = self.single_models[peak.prefix]
+        return model.eval(params=self.params, x=self.region.energy)
+
+    def get_intensity(self):
+        """Returns overall fit result."""
+        if not self.total_model:
+            return None
+        return self.total_model.eval(params=self.params, x=self.region.energy)
+
+    def add_constraint(self, peak, param, **kwargs):
+        """Adds a constraint to a Peak parameter."""
+        param_name = peak.prefix + param
+        self.params[param_name].set(**kwargs)
+
+    def add_relation(self):
+        """Relates two parameters."""
+        pass
